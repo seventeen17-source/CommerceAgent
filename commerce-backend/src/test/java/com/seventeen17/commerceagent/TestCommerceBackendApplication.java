ef736ba05e0a9ef51063689209617b8fcbc6b16d
seventeen17-source <2896791746@qq.com>
2026-09-16 22:08:08 +0800

    feat(setup): add verified Spring Boot backend scaffold

---
+++ commerce-backend/src/test/java/com/seventeen17/commerceagent/TestCommerceBackendApplication.java
@@ -0,0 +1,11 @@
+package com.seventeen17.commerceagent;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(CommerceBackendApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
