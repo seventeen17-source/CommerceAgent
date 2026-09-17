2a5dbde62dad06e4cef57b9b62799786da4faf50
seventeen17-source <2896791746@qq.com>
2026-09-17 16:06:06 +0800

    chore(backend): add spotless, spotbugs and jacoco

--- commerce-backend/src/test/java/com/seventeen17/commerceagent/TestCommerceBackendApplication.java
+++ commerce-backend/src/test/java/com/seventeen17/commerceagent/TestCommerceBackendApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 
 public class TestCommerceBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.from(CommerceBackendApplication::main).with(TestcontainersConfiguration.class).run(args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.from(CommerceBackendApplication::main)
+                .with(TestcontainersConfiguration.class)
+                .run(args);
+    }
 }
